07751d4da610076168fdd2907e60e21596e2363f
lifewhiz <arnav.nidumolu@gmail.com>
2024-10-11 17:19:12 -0700

    init

---
+++ scripts/clone_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+gh repo list GreenPlanet-Capital --visibility public --limit 4000 | while read -r repo _; do
+    if [[ "$repo" == "GreenPlanet-Capital/AlgoBot" ||  "$repo" == "GreenPlanet-Capital/jessify-strategies" ||  "$repo" == "GreenPlanet-Capital/GreenPlanetCapital" ]]; then
+        continue
+    fi
+    echo "Cloning $repo"
+    gh repo clone "$repo" -- --recurse-submodules
+done
+
+mkdir -p marketstore/data
+cp config/mkts.yml marketstore
